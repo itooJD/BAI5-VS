684ebe18ecd6940f9d86e530bb12c41f72c046d7
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-27 17:46:56 +0100

    Config udp 1.0

--- utils/config_manager.py
+++ utils/config_manager.py
@@ -25,16 +25,20 @@ def write_config(paths):
 def get_server_url():
     s=socket(AF_INET, SOCK_DGRAM)
     s.bind(('',24000))
-    address = s.recvfrom(1024 )
-    print('Config: Received {0} as address'.format(address))
+    udp_received = s.recvfrom(1024 )
+    port_pre = udp_received[0]
+    address_pre  = udp_received[1]
+    port = port_pre.encode('utf-8')
+    address = address_pre[0]
+    print('Config: Received {0} as address and {1} as port'.format(address, port))
     return address
 
 
 def set_server_url_via_udp():
     print('Config: Setting server_url')
     paths = get_config()
-    server =  get_server_url()
-    server_url = 'http://{0}:5000'.format(server)
+    server, port =  get_server_url()
+    server_url = 'http://{0}:{1}'.format(server, port)
     paths['server'] =  server_url
     write_config(paths)
     return paths
