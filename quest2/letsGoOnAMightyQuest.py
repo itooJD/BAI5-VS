9cd47e4983a7e340ee94769777fe3bd776197490
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-27 18:03:00 +0100

    MightyQuest Bugfix 0.1

--- quest2/letsGoOnAMightyQuest.py
+++ quest2/letsGoOnAMightyQuest.py
@@ -1,9 +1,9 @@
 import requests
 
-from quest2.questing_resources import blackboard as bb
-from quest2.questing_resources import users, game_map, taverna, group
-from quest2.questing_resources.authentification import authentification as auth, logout
-from quest2.questing_resources.quest import my_quest
+from questing_resources import blackboard as bb
+from questing_resources import users, game_map, taverna, group
+from questing_resources.authentification import authentification as auth, logout
+from questing_resources.quest import my_quest
 from utils import paths, serializer as ser, outputs
 
 
