4f9608510d4990de96e083eed07d46204234235e
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-29 00:54:47 +0100

    S+UI 0.1

--- quests/run.py
+++ quests/run.py
@@ -1,14 +1,17 @@
-from quests.quest2.server_setup import setup_flask, add_api
-from quests.utils import set_server_url_via_udp
-from threading import Thread
 
 if __name__ == '__main__':
+    from quests.quest2.server_setup import setup_flask, add_api
+    from quests.utils import set_server_url_via_udp
+    from threading import Thread
     print('Setup: Setting server url')
     paths = set_server_url_via_udp()
+    print('Setup: Configuring flask-server')
     app, api = setup_flask()
+    print('Setup: Adding Rest-API')
     add_api(paths, api)
-    print('')
+    print('Setup: Starting flask-server')
     thread = Thread(app.run(host='0.0.0.0', debug=True))
     thread.start()
+    print('Setup: Starting UI')
     from quests.quest1 import quest1
     thread.join()
