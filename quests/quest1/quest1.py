eeaa9397b082a95229359d9073ba3856b196bc92
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-28 21:19:04 +0100

    Quest1 Extension 0.2.9

--- quests/quest1/quest1.py
+++ quests/quest1/quest1.py
@@ -79,10 +79,11 @@ def visit(paths, headers, quest_host, location_url):
     return {'tokens': {'/blackboard/tasks/2': throneroom_token}}
 
 def deliver(paths, headers, deliver_token, task):
+    print()
+    print('Quest1: Now let us deliver our token. Back to the blackboard!')
     last_resp = requests.post(paths['server'] + paths['blackboard_url'] + paths['task_url'] + '/' + task + paths['deliver_url'], headers=headers, data=deliver_token)
     print(last_resp.json()['message'])
-    # print(last_resp.json()[''])
-    print("Quest successfully closed")
+    print("Quest successfully closed! Herrrrroooooooooy Jeeeeenkiiiiiins!!")
 
 if __name__ == '__main__':
     paths = set_server_url_via_udp()
