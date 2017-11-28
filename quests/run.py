611299c5d9b867ad5ab26753306de67e47fc691f
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-29 00:56:08 +0100

    S+UI 0.1.1

--- quests/run.py
+++ quests/run.py
@@ -1,7 +1,7 @@
 
 if __name__ == '__main__':
-    from quests.quest2.server_setup import setup_flask, add_api
-    from quests.utils import set_server_url_via_udp
+    from .quest2.server_setup import setup_flask, add_api
+    from .utils import set_server_url_via_udp
     from threading import Thread
     print('Setup: Setting server url')
     paths = set_server_url_via_udp()
@@ -13,5 +13,5 @@ if __name__ == '__main__':
     thread = Thread(app.run(host='0.0.0.0', debug=True))
     thread.start()
     print('Setup: Starting UI')
-    from quests.quest1 import quest1
+    from .quest1 import quest1
     thread.join()
