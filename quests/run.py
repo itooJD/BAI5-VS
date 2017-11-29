fd606f4e2989dbc0f1485d261526818adfb99ac6
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-29 01:01:02 +0100

    S+UI 0.1.3

--- quests/run.py
+++ quests/run.py
@@ -11,7 +11,3 @@ if __name__ == '__main__':
     add_api(paths, api)
     print('Setup: Starting flask-server')
     thread = Thread(app.run(host='0.0.0.0', debug=True))
-    thread.start()
-    print('Setup: Starting UI')
-    from quests.quest1 import quest1
-    thread.join()
