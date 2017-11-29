19b3bc0d6ec41e5936b4f3c41808ba632d009c45
Till Pohland <till.pohland@haw-hamburg.de>
2017-11-29 13:36:47 +0100

    Main UI 0.2.6

--- quests/quest1/ui.py
+++ quests/quest1/ui.py
@@ -1,5 +1,8 @@
-from quests.quest1.ui_filter import *
 from quests.utils import get_config
+from quests.quest1.questing import search_location
+from quests.quest1.blackboard import show_users, show_all_quests, show_available_quests, choose_quest
+from quests.quest1.utilities import logout
+
 
 
 def main_ui(auth_header, quest):
@@ -29,6 +32,26 @@ def quest_ui(auth_header):
     print('2: All')
     print('Else: Exit')
     return quest_filter(input('> '), auth_header)
+
+
+def main_filter(choice, auth_header):
+    choice_filter = {
+        '1': choose_quest,
+        '3': show_users,
+        '4': search_location,
+        '5': logout
+    }
+    return choice_filter.get(choice)(auth_header)
+
+
+def quest_filter(choice, auth_header):
+    choice_filter = {
+        '1': show_available_quests,
+        '2': show_all_quests
+    }
+    return choice_filter.get(choice)(auth_header)
+
+
 
 
 def taverna_ui():
