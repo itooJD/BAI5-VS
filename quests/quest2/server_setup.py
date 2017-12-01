c7ee6063d624bbd60f2d8193c68e83db5aef64fb
Till Pohland <till.pohland@haw-hamburg.de>
2017-12-02 00:42:42 +0100

    Server Update 0.2

--- quests/quest2/server_setup.py
+++ quests/quest2/server_setup.py
@@ -7,11 +7,11 @@ def setup_flask():
     api = Api(app)
     return app, api
 
-from quests.quest2.resources import HelloToHeroysWorld, Heroy, HeroysDiary, HeroysMightyTasks, HeroyPresident
+from quests.quest2.resources import HelloToHeroysWorld, HeroyJenkins, HeroysDiary, HeroysMightyTasks, HeroyPresident
 
 def add_api(paths, api):
     api.add_resource(HelloToHeroysWorld, '/')
-    api.add_resource(Heroy, paths['hero_url'])
+    api.add_resource(HeroyJenkins, paths['hero_url'])
     api.add_resource(HeroysDiary, paths['diary_url'])
     api.add_resource(HeroysMightyTasks, paths['assignment_url'])
     api.add_resource(HeroyPresident, paths['election_url'])
