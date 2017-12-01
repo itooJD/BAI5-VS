f478892043c2c4eb63d9322c3bff98da4e4faf27
Till Pohland <till.pohland@haw-hamburg.de>
2017-12-01 22:05:59 +0100

    Main UI 0.4.22

--- quests/quest1/taverna/groups.py
+++ quests/quest1/taverna/groups.py
@@ -110,6 +110,8 @@ def check_members(auth_header, groups):
             group_existant = True
         if group_existant:
             response = requests.get(paths_util.group_url_id(group_id) + get_config()['member_url'], headers=auth_header)
+            print(response.json())
+            print('Members: ')
             for member in response.json()['objects']:
                 if member.get('heroclass'):
                     print(member.get('heroclass') + ' | ', end='')
