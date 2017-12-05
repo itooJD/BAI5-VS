3a7b2e4020fc7079e07935e980bd84768db3c1c7
vicid <itoo.anders@gmail.com>
2017-12-05 16:01:22 +0100

    Auto stash before merge of "master" and "zujiri"

--- quests/utils/__init__.py
+++ quests/utils/__init__.py
@@ -1,2 +1,4 @@
-__all__ = ["get_config", "set_server_url_via_udp", 'change_config', 'add_to', 'rm_from', 'set_own_url']
+__all__ = ["get_config", "set_server_url_via_udp", 'change_config', 'add_to', 'rm_from', 'set_own_url',
+           "election_algorithm"]
 from .config_manager import get_config, change_config, set_server_url_via_udp, add_to, rm_from, set_own_url
+from .election_algorithm import election_algorithm
