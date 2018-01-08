ccc7607932ecfc713e0cb0e7acc39ecb38b57f20
vicid <itoo.anders@gmail.com>
2018-01-08 22:13:41 +0100

    Mutex 0.0.16

--- quests/server/resources/HeroysMutex.py
+++ quests/server/resources/HeroysMutex.py
@@ -19,7 +19,7 @@ class HeroysMutex(Resource):
     def post(self):
         json_data = request.get_json(force=True)
         config = get_config()
-        lamport_clock = config['lamport_clock'] + 1
+        lamport_clock = config['lamport_clock']
         state = config['state']
         stored_requests = config['stored_requests']
         remote_addr = request.remote_addr
@@ -35,22 +35,13 @@ class HeroysMutex(Resource):
                         stored_requests.append(remote_addr)
                         change_config('stored_requests', stored_requests)
                     message = 'request'
-                '''
-                elif state == 'wanting' and json_data['time'] < lamport_clock:
-                    if remote_addr not in stored_requests:
-                        stored_requests.append(remote_addr)
-                        change_config('stored_requests', stored_requests)
-                    message = 'request'
-                elif state == 'held':
-                    if remote_addr not in stored_requests:
-                        stored_requests.append(remote_addr)
-                        change_config('stored_requests', stored_requests)
-                    message = 'request'
-                '''
                 response = {
                     'msg': message,
                     'time': lamport_clock
                 }
+                if json_data['time'] > lamport_clock:
+                    lamport_clock = json_data['time']
+                lamport_clock += 1
                 change_config('lamport_clock', lamport_clock)
                 return jsonify(response)
             else:
@@ -58,38 +49,52 @@ class HeroysMutex(Resource):
         except KeyError or TypeError:
             return abort(400)
 
+    # change status
+    # {
+    #   "message":"state",
+    #   "state":"released" oder....
+    # }
+    #
+    # update clock
+    # {
+    #   "message":"clock"
+    # }
     def put(self):
         json_data = request.get_json(force=True)
         config = get_config()
         lamport_clock = config['lamport_clock']
         state = config['state']
-        stored_requests = config['stored_requests']
         try:
             message = 'update unsuccessful, {state:' + str(state) + ',clock:' + str(lamport_clock) + '}'
-            if bool(json_data) and len(json_data) == 2:
-                if json_data['message'] == 'state' and json_data['state'] in self.states:
-                    if state != 'released' and json_data['state'] == 'released':
-                        print('stored requets: ' + str(stored_requests))
-                        self.answer_stored_requests(stored_requests)
-                        change_config('stored_reqeuests', list())
-                    state = json_data['state']
-                    change_config('state', state)
-                    message = 'successfully update state to ' + str(state)
-                if json_data['message'] == 'clock':
-                    lamport_clock += 1
-                    change_config('lamport_clock', lamport_clock)
-                    message = 'sucessufully update clock to ' + str(lamport_clock)
+            if json_data['message'] == 'state' and json_data['state'] in self.states and len(json_data) == 2:
+                if json_data['state'] == 'released':
+                    if state != 'released':
+                        self.answer_stored_requests()
+                    change_config('stored_reqeuests', list())
+                state = json_data['state']
+                change_config('state', state)
+                message = 'successfully update state to ' + str(state)
+            if json_data['message'] == 'clock' and len(json_data) == 1:
+                lamport_clock += 1
+                change_config('lamport_clock', lamport_clock)
+                message = 'sucessufully update clock to ' + str(lamport_clock)
             response = {'msg': message}
             return jsonify(response)
         except KeyError or TypeError:
             return abort(400)
 
-    def store_request(self):
-        pass
-
-    def answer_stored_requests(self, stored_requests):
-        print('ich bin in answer_stored_requests')
+    def answer_stored_requests(self):
+        config = get_config()
+        lamport_clock = config['lamport_clock']
+        stored_requests = config['stored_requests']
+        # in stored_request are only the ip-addresses
+        # get adventurer list
         for request in stored_requests:
-            print('Answer Request: ' + request)
-            requests.post('http://' + request + ':5000/path', data='{"msg":"reply-ok","time":1}')
-        pass
+            data = {
+                "msg": "reply-ok",
+                "time": lamport_clock
+            }
+            # get full URL of request from adventurer list
+            # send POST to mutex with data
+            lamport_clock += 1
+        change_config('lamport_clock', lamport_clock)
--- quests/utils/mutex_algorithm.py
+++ quests/utils/mutex_algorithm.py
@@ -18,12 +18,23 @@ def enter_critical_section():
     }
     requests.put('http://' + str(own_address) + ':' + str(5000), data=state)
 
+    # get adventurers list
     adventurers = list()
     for adventurer in adventurers:
+        # send post request to adventurer
         response = requests.post(adventurer, data=request, timeout=0.2)
         data = response.json()
         if data['msg'] != 'reply-ok' or response.status_code != 200:
             waiting_answers.append(adventurer)
         lamport_clock += 1
+
     change_config('waiting_answers', waiting_answers)
     change_config('lamport_clock', lamport_clock)
+
+    while len(waiting_answers) > 0:
+        waiting_answers = config['waiting_answers']
+    state["state"] = "held"
+    requests.put('http://' + str(own_address) + ':' + str(5000), data=state)
+    # do something
+    state["state"] = "released"
+    requests.put('http://' + str(own_address) + ':' + str(5000), data=state)
