9ab32f2f03d4adf74f2bd36d8ca99a78c28763af
Till Pohland <till.pohland@haw-hamburg.de>
2018-01-09 21:32:43 +0100

    Mutex UI 0.9.6

--- quests/server/resources/HeroysMutex.py
+++ quests/server/resources/HeroysMutex.py
@@ -1,8 +1,7 @@
 import requests
 from flask import jsonify, request, abort
 from flask_restful import Resource
-
-from quests.utils import get_config, change_config
+from quests.utils import get_config, change_config, add_to
 
 
 class HeroysMutex(Resource):
@@ -32,6 +31,7 @@ class HeroysMutex(Resource):
                     'time': lamport_clock
                 }
             elif json_data['msg'] == 'request' and len(json_data) == 4:
+                print('Received Reqest')
                 waiting_answers = config['waiting_answers']
                 if json_data['user'] in waiting_answers:
                     waiting_answers.remove(json_data['user'])
@@ -42,6 +42,7 @@ class HeroysMutex(Resource):
                     stored_requests.append(json_data['reply'])
                     message = 'request'
             else:
+                print(str(json_data))
                 return abort(400)
 
             if json_data['time'] > lamport_clock:
