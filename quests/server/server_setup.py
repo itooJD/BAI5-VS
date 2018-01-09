05f1d55fc5ebd5c195e92f1763ad0e07f5cb525b
Till Pohland <till.pohland@haw-hamburg.de>
2018-01-09 18:01:56 +0100

    Mutex UI 0.7.15

--- quests/server/server_setup.py
+++ quests/server/server_setup.py
@@ -10,11 +10,10 @@ def setup_flask():
 
 
 def add_api(paths, api):
-    pass
-    #api.add_resource(Heroy, paths['hero_url'])
-    #api.add_resource(HeroysDiary, paths['diary_url'])
-    #api.add_resource(HeroysMightyTasks, paths['assignment_url'])
-    #api.add_resource(HeroyPresident, paths['election_url'])
-    #api.add_resource(HeroysCallMeMaybeBack, paths['callback_url'])
-    # api.add_resource(HeroysMutex, '/path') # must do
-    # api.add_resource(TheFake, '/mutex')
+    api.add_resource(Heroy, paths['hero_url'])
+    api.add_resource(HeroysDiary, paths['diary_url'])
+    api.add_resource(HeroysMightyTasks, paths['assignment_url'])
+    api.add_resource(HeroyPresident, paths['election_url'])
+    api.add_resource(HeroysCallMeMaybeBack, paths['callback_url'])
+    api.add_resource(HeroysMutex, '/path') # must do
+    api.add_resource(TheFake, '/mutex')
